3e1675ddf58b2ef816437ff898da53eadd60e06b
inno-leokim <inno.leokim@gmail.com>
2024-02-05 13:17:49 +0900

    Edit threading.Thread arg

--- thread_02.py
+++ thread_02.py
@@ -6,7 +6,7 @@ def getHtml(url):
     time.sleep(1)
     print(url, len(resp.text), ' chars')
  
-t1 = threading.Thread(target=getHtml, args=('http://google.com',))
+t1 = threading.Thread(target=getHtml, args=('http://google.com',)) # args는 튜플로 전달되어야 한다. 따라서 ','를 붙여줬다. 그렇지 않으면 string 타입이 된다.
 t1.start()
  
 print("### End ###")
